4a42752ff324bf41f1c5532ef70cb3502fac6b65
Eduardo Ramirez <Eduardo.rmz98@gmail.com>
2023-08-16 11:53:44 -0700

    Se creó el archivo de la cuarta práctica; la cual es sobre los objetos. Se hicieron los distintos ejercicios que fueron requeridos

---
+++ Práctica 4 - Objetos/Práctica 4.js
@@ -0,0 +1,29 @@
+
+let persona = {
+    nombre: 'Eduardo',
+    edad: 25,
+    profesion: 'Estudiante'
+}
+
+let libro = {
+    titulo: "Lestat",
+    autor: "Anne Rice"
+}
+
+libro.autor = "Marian Rojas"
+
+console.log(libro.titulo)
+
+let mascota = {
+    nombre: "Kira",
+    raza: "Labrador",
+    edad: 10
+}
+
+let {nombre,raza,edad} = mascota
+
+console.log(`Nombre: ${nombre}\n Raza: ${raza}\n Edad: ${edad}`)
+
+
+
+
