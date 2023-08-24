0e339c02495997aeba51f04fb46688d1c40c1475
Eduardo Ramirez <Eduardo.rmz98@gmail.com>
2023-08-24 11:23:19 -0700

    Ejercicio de saltos de rana terminado

---
+++ Práctica 6 - Rana/Práctica 6.js
@@ -0,0 +1,11 @@
+
+function SaltosDeRana(x,y,d){
+    let saltos = 0
+    while (x<=y){
+        saltos++
+       x += d
+   }
+    console.log(`Los saltos que dio fueron ${saltos} y llegó a la distancia de ${x} `)
+}
+
+SaltosDeRana(20,85,30)
